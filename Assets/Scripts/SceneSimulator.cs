6479e26b713005f8f7e3ad86aad5746ba0881b7d
ConnorChow <connorjameschow@gmail.com>
2023-01-30 11:24:26 -0500

    Working on Multithreaded capabilities

--- Assets/Scripts/SceneSimulator.cs
+++ Assets/Scripts/SceneSimulator.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Unity.Jobs;
+using Unity.Collections;
+//using UnityEngine.WSA;
 
 public struct WFCTile
 {
@@ -16,6 +19,28 @@ public struct WFCTile
     public Tile GetTile()
     {
         return tile;
+    }
+    public int[] GetSockets()
+    {
+        return sockets;
+    }
+    public void SetSockets(int direction, int SocketType)
+    {
+        this.sockets[direction] = SocketType;
+    }
+}
+
+public struct UnmanagedTile
+{
+    public int tileRef;
+    int[] sockets;
+    public int GetTile()
+    {
+        return tileRef;
+    }
+    public void SetTile(int t)
+    {
+        tileRef= t;
     }
     public int[] GetSockets()
     {
@@ -50,7 +75,7 @@ public class SceneSimulator : MonoBehaviour {
     static int avoid = 1;
     static int obstacle = 2;
 
-    WFCTile[] tiles = new WFCTile[18];
+    NativeArray<WFCTile> tiles;
 
     [Header("Ground TileTypes")]
     public Tile DirtFull;
@@ -75,11 +100,91 @@ public class SceneSimulator : MonoBehaviour {
     public Tile DirtGrassDownRight;
 
     [Header("Tile Maps")]
-    public int TerrainSize = 16;
-    WFCTile[,] Map2D;
+    public readonly int TerrainSize = 16;
+    //WFCTile[,] Map2D;
+    NativeArray<WFCTile> Map2D;
     public Tilemap GroundTileMap;
 
-    private void CollapseTerrain(int posx, int posy) {
+    public struct CollapseJob : IJob
+    {
+        public int posx;
+        public int posy;
+        public int TerrainSize;
+        public NativeArray<WFCTile> Map2D;
+        public NativeArray<WFCTile> tiles;
+        public Tilemap GroundTileMap;
+        public void Execute()
+        {
+            CollapseTerrain(posx, posy);
+        }
+        private void CollapseTerrain(int posx, int posy)
+        {
+            if (posx < TerrainSize && posy < TerrainSize)
+            {
+                int[] TileOptions = new int[18];
+                int count = 0;
+                //get tiles around inst
+                if (posx > 0)
+                {
+                    Map2D[posx * TerrainSize + posy].SetSockets(
+                        left,
+                        Map2D[(posx - 1) * TerrainSize + posy].GetSockets()[right]
+                    );
+                }
+                if (posx < TerrainSize - 1)
+                {
+                    Map2D[posx * TerrainSize + posy].SetSockets(
+                        right,
+                        Map2D[(posx + 1) * TerrainSize + posy].GetSockets()[left]
+                    );
+                }
+                if (posy < TerrainSize - 1)
+                {
+                    Map2D[posx * TerrainSize + posy].SetSockets(
+                        up,
+                        Map2D[posx * TerrainSize + posy + 1].GetSockets()[down]
+                    );
+                }
+                if (posy > 0)
+                {
+                    Map2D[posx * TerrainSize + posy].SetSockets(
+                        down,
+                        Map2D[posx * TerrainSize + posy - 1].GetSockets()[up]
+                    );
+                }
+                //find tiletype matching socket
+                for (int type = 0; type < tiles.Length; type++)
+                {
+                    if (Map2D[posx * TerrainSize + posy].GetSockets()[left] == tiles[type].GetSockets()[left] || Map2D[posx * TerrainSize + posy].GetSockets()[left] == Empty)
+                    { }
+                    else { continue; }
+                    if (Map2D[posx * TerrainSize + posy].GetSockets()[right] == tiles[type].GetSockets()[right] || Map2D[posx * TerrainSize + posy].GetSockets()[right] == Empty)
+                    { }
+                    else { continue; }
+                    if (Map2D[posx * TerrainSize + posy].GetSockets()[up] == tiles[type].GetSockets()[up] || Map2D[posx * TerrainSize + posy].GetSockets()[up] == Empty)
+                    { }
+                    else { continue; }
+                    if (Map2D[posx * TerrainSize + posy].GetSockets()[down] == tiles[type].GetSockets()[down] || Map2D[posx * TerrainSize + posy].GetSockets()[down] == Empty)
+                    { }
+                    else { continue; }
+
+                    if (type <= 1 && Random.Range(0, 10) < 1)
+                        continue;
+
+                    TileOptions[count] = type;
+                    count++;
+                }
+                //set tile and socket
+                int RandomFittingTile = Random.Range(0, count - 1);
+                Map2D[posx * TerrainSize + posy] = tiles[TileOptions[RandomFittingTile]];
+
+                //GroundTileMap.SetTile(new Vector3Int(posx - (TerrainSize / 2), posy - (TerrainSize / 2), 0), Map2D[posx * TerrainSize + posy].GetTile());
+            }
+
+        }
+    }
+
+    /*private void CollapseTerrain(int posx, int posy) {
         if (posx < TerrainSize && posy < TerrainSize)
         {
             int[] TileOptions = new int[18];
@@ -136,29 +241,12 @@ public class SceneSimulator : MonoBehaviour {
             Map2D[posx, posy] = tiles[TileOptions[RandomFittingTile]];
 
             GroundTileMap.SetTile(new Vector3Int(posx - (TerrainSize/2), posy - (TerrainSize/2), 0), Map2D[posx, posy].GetTile());
-
-            /*if (posx < TerrainSize - 1)
-            {
-                CollapseTerrain(posx + 1, posy);
-            }
-            if (posx > 0)
-            {
-                CollapseTerrain(posx - 1, posy);
-            }
-            if (posy < TerrainSize - 1)
-            {
-                CollapseTerrain(posx, posy + 1);
-
-            }
-            if (posy > 0)
-            {
-                CollapseTerrain(posx, posy - 1);
-            }*/
         }
 
-    }
+    }*/
     private void InitTerrain()
     {
+        tiles = new NativeArray<WFCTile>(18, Allocator.TempJob);
         //[0] = left, [1] = up, [2] = right, [3] = down
         //GrassVDirt = Grass on down/left, Dirt on up/right
         tiles[0].setParams(DirtFull, new int[4] { Dirt, Dirt, Dirt, Dirt });
@@ -182,18 +270,36 @@ public class SceneSimulator : MonoBehaviour {
 
         Random.InitState((int)Time.time);
 
-        Map2D = new WFCTile[TerrainSize, TerrainSize];
+        Map2D = new NativeArray<WFCTile>(TerrainSize * TerrainSize, Allocator.TempJob);//new WFCTile[TerrainSize, TerrainSize];
 
         for (int x = 0; x < TerrainSize; x++) {
             for (int y = 0; y < TerrainSize; y++) {
-                Map2D[x, y].setParams(null, new int[4] { Empty, Empty, Empty, Empty });
+                Map2D[x * TerrainSize + y].setParams(null, new int[4] { Empty, Empty, Empty, Empty });
             }
         }
-        for (int x = 0; x < TerrainSize; x++)
+        JobHandle CommitCollapse;
+        CollapseJob collapseXY = new CollapseJob
         {
+            posx = 0,
+            posy = 0,
+            TerrainSize = TerrainSize,
+            Map2D = Map2D,
+            tiles = tiles,
+            GroundTileMap = GroundTileMap
+        };
+        JobHandle PrevCollapse = collapseXY.Schedule();
+        for (int x = 0; x < TerrainSize; x++) {
             for (int y = 0; y < TerrainSize; y++)
             {
-                CollapseTerrain(x, y);
+                //CollapseTerrain(x, y);
+                collapseXY.posx= x;
+                collapseXY.posy= y;
+                if (x > 0 && y > 0)
+                {
+                    CommitCollapse = collapseXY.Schedule(PrevCollapse);
+                    PrevCollapse = CommitCollapse;
+                }
+                GroundTileMap.SetTile(new Vector3Int(x - (TerrainSize / 2), x - (TerrainSize / 2), 0), collapseXY.Map2D[x * TerrainSize + y].GetTile());
             }
         }
         //CollapseTerrain(0, 0);
