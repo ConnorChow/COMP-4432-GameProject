a5dc7fbd6862eda7f744ac73f8ba993f6e273122
Mia Zhang <zhangmuyao666@gmail.com>
2023-02-11 16:31:12 -0500

    update code parts and file location of boss and player

--- Assets/Editor/CreateAssetBundles.cs
+++ Assets/Editor/CreateAssetBundles.cs
@@ -9,7 +9,7 @@ public class CreateAssetBundles : MonoBehaviour
     [MenuItem("Assets/Build AssetBundles")]
     static void BuildAllAssetBundles()
     {
-        string assetBundleDirectory = "Assets/assetBundles";
+        string assetBundleDirectory = "Assets/AssetBundles";
         if(!Directory.Exists(assetBundleDirectory))
         {
             Directory.CreateDirectory(assetBundleDirectory);
@@ -22,15 +22,30 @@ public class CreateAssetBundles : MonoBehaviour
     public class LoadFromFileExample : MonoBehaviour {
     IEnumerator InstantiateObject()
 {
-    string url = "file:///" + Application.dataPath + "/AssetBundles/" + "Boss";        
+    string url = "file:///" + Application.dataPath + "/AssetBundles/" + "Boss" + "Player";        
     var request 
         = UnityEngine.Networking.UnityWebRequestAssetBundle.GetAssetBundle(url, 0);
     yield return request.Send();
     AssetBundle bundle = UnityEngine.Networking.DownloadHandlerAssetBundle.GetContent(request);
+
+    GameObject Hero = bundle.LoadAsset<GameObject>("Hero");
+    GameObject HeroWalk = bundle.LoadAsset<GameObject>("Hero walk");
+    GameObject HeroUseItem = bundle.LoadAsset<GameObject>("Hero use item");
+    GameObject HeroPlayer2 = bundle.LoadAsset<GameObject>("Hero Player 2");
+    GameObject HeroPlayer2UseItem = bundle.LoadAsset<GameObject>("Hero Player 2 use item");
+    GameObject HeroPlayer2walk = bundle.LoadAsset<GameObject>("Hero Player 2 walk");
     GameObject demonwalk = bundle.LoadAsset<GameObject>("Demon topdown walk");
     GameObject demonattack = bundle.LoadAsset<GameObject>("Demon topdown attack");
+    GameObject demonview = bundle.LoadAsset<GameObject>("Demon topdown view");
+    Instantiate(Hero);
+    Instantiate(HeroWalk);
+    Instantiate(HeroUseItem);
+    Instantiate(HeroPlayer2);
+    Instantiate(HeroPlayer2UseItem);
+    Instantiate(HeroPlayer2walk);
     Instantiate(demonwalk);
     Instantiate(demonattack);
+    Instantiate(demonview);
         }   
     }
 }
