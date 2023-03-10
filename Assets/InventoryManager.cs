af5bed918cdfac13073cea74d8b21d166142224c
Mia Zhang <zhangmuyao666@gmail.com>
2023-03-09 22:38:12 -0500

    Inventory UI almost done

---
+++ Assets/InventoryManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject slotPrefab;
+    public List<InventorySlot> InventorySlots = new List<InventorySlot>(12);
+
+    private void OnEnable()
+    {
+        Inventory.OnInventoryChange += DrawInventory;
+    }
+
+    private void OnDisable()
+    {
+        Inventory.OnInventoryChange -= DrawInventory;
+    }
+
+    void ResetInventory()
+    {
+        foreach(Transform childTransform in transform)
+        {
+            Destroy(childTransform.gameObject);
+        }
+        InventorySlots = new List<InventorySlot>(12);
+    }
+
+    void DrawInventory(List<InventoryItem> inventory)
+    {
+        ResetInventory();
+
+        for (int i = 0; i < InventorySlots.Capacity; i++)
+        {
+            CreateInventory();
+        }
+
+        for (int i=0; i< inventory.Count;i++)
+        {
+            InventorySlots[i].DrawSlot(inventory[i]);
+        }
+    }
+
+    void CreateInventory()
+    {
+        GameObject newSlot = Instantiate(slotPrefab);
+        newSlot.transform.SetParent(transform, false);
+
+        InventorySlot newSlotComponent = newSlot.GetComponent<InventorySlot>();
+        newSlotComponent.ClearSlot();
+
+        InventorySlots.Add(newSlotComponent);
+    }
+}
