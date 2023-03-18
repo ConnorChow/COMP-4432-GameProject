37cd09961ca234fd0574263c824fe613e270a532
Mia Zhang <zhangmuyao666@gmail.com>
2023-03-18 01:28:23 -0400

    .

--- Assets/REST API/RESTAPI endpoint.js
+++ Assets/REST API/RESTAPI endpoint.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('Asset/Characters/Player', (req, res) => {
-    const Players = ['Hero', 'Hero Player 2'];
+    const Players = ['Hero', 'Hero Player 2', 'Player 2 Knight'];
     const randomIndex = Math.floor(Math.random() * players.length);
     const randomPlayers = Players[randomIndex];
     res.send(randomPlayers);
