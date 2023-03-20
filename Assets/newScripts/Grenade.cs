aaefe2f0e9fb3a6129d935a9eeb9660c668ea0cf
ConnorChow <connorjameschow@gmail.com>
2023-03-20 11:02:10 -0400

    Added ability to set fires

--- Assets/newScripts/Grenade.cs
+++ Assets/newScripts/Grenade.cs
@@ -2,38 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grenade : MonoBehaviour
-{
+public class Grenade : MonoBehaviour {
     private Vector2 targetPos;
 
     public float speed = 5;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         targetPos = GameObject.Find("firePoint").transform.position;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(speed > 0)
-        {
-            speed -= Random.Range(.1f, .25f);
+    void Update() {
+        if (speed > 0) {
+            speed -= Random.Range(.1f, .25f) * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-        }
-        else if (speed < 0)
-        {
+        } else if (speed <= 0) {
             speed = 0;
-            //Add explosion
+            Explode();
         }
+    }
+
+    void Explode() {
+        LandscapeSimulator landscape = GameObject.Find("SceneSimulator").GetComponent<LandscapeSimulator>();
+        if (landscape != null) {
+            landscape.BurnCellFromV2(new Vector2(transform.position.x, transform.position.y));
+        }
+        Destroy(gameObject, 5);
     }
 
 
 
     //add this to player movement: 
     //public GameObject grenade;
-    
+
     //if (Input.GetKeyDown("e"))
     //{
     //Instantiate(grenade, transform.position, Quaternion.identity);    
