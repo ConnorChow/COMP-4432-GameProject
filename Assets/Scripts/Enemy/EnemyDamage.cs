ace2aba84dc151511423d717234e111b1e46da23
ktbrochu <123405854+ktbrochu@users.noreply.github.com>
2023-03-27 16:20:55 -0400

    Update EnemyDamage.cs

--- Assets/Scripts/Enemy/EnemyDamage.cs
+++ Assets/Scripts/Enemy/EnemyDamage.cs
@@ -18,11 +18,19 @@ public class EnemyDamage : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    /* private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             playerHealth.TakeDamage(damage);
+        }
+    }
+    */
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Player>().TakeDamage(damage);
         }
     }
 }
