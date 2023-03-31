985dd2c5ecbe9ccb03c57f2c96fcec310d326f2d
Mia Zhang <zhangmuyao666@gmail.com>
2023-03-31 12:27:46 -0400

    weather object added in the main scene and some changes for GetLocation

--- Assets/Weather/GetLocation.cs
+++ Assets/Weather/GetLocation.cs
@@ -17,6 +17,7 @@ public class GetLocation : MonoBehaviour
 	}
 	private IEnumerator GetIP()
 	{
+		/*
 		var www = new UnityWebRequest("https://ipapi.co/json/")
 		{
 			downloadHandler = new DownloadHandlerBuffer()
@@ -31,11 +32,23 @@ public class GetLocation : MonoBehaviour
 		}
 
 		IPAddress = www.downloadHandler.text;
-		StartCoroutine (GetCoordinates());
+		StartCoroutine (GetCoordinates());*/
+		var www = UnityWebRequest.Get("https://ipapi.co/json/");
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(www.error);
+            yield break;
+        }
+
+        IPAddress = www.downloadHandler.text;
+        StartCoroutine(GetCoordinates());
 	}
 
 	private IEnumerator GetCoordinates()
 	{
+		/*
 		//string encodedIPAddress = Uri.EscapeDataString(IPAddress);
 		var www = new UnityWebRequest("https://ipapi.co/json/" + IPAddress)
 		{
@@ -53,14 +66,30 @@ public class GetLocation : MonoBehaviour
 		Info = JsonUtility.FromJson<LocationInfo>(www.downloadHandler.text);
 		latitude = Info.lat;
 		longitude = Info.lon;
+		IPAddress = Info.ip;
 		weatherData.Begin ();
-	}
+	}*/
+		var www = UnityWebRequest.Get("https://ipapi.co/" + IPAddress + "/json/");
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(www.error);
+            yield break;
+        }
+
+        Info = JsonUtility.FromJson<LocationInfo>(www.downloadHandler.text);
+        latitude = Info.latitude;
+        longitude = Info.longitude;
+        IPAddress = Info.ip;
+        weatherData.Begin();
+    }
 }
 
 [Serializable]
 public class LocationInfo
 {
-	public string status;
+	/*public string status;
 	public string country;
 	public string countryCode;
 	public string region;
@@ -73,5 +102,32 @@ public class LocationInfo
 	public string isp;
 	public string org;
 	public string @as;
-	public string query;
+	public string query;*/
+	public string ip;
+	public string network;
+	public string version;
+	public string city;
+	public string region;
+	public string region_code;
+	public string country;
+	public string country_name;
+	public string country_code;
+	public string country_code_iso3;
+	public string country_capital;
+	public string country_tld;
+	public string continent_code;
+	public string in_eu;
+	public string postal;
+	public float latitude;
+    public float longitude;
+	public string timezone;
+	public string utc_offset;
+	public string country_calling_code;
+	public string currency;
+	public string currency_name;
+	public string languages;
+	public string country_area;
+	public string country_population;
+	public string asn;
+	public string org;
 }
