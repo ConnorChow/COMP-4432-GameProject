3b08c2c277e0847c05165fe5b5446b4c30384942
Mia Zhang <zhangmuyao666@gmail.com>
2023-03-30 23:56:24 -0400

    weather parts and codes

---
+++ Assets/Weather/GetLocation.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+
+public class GetLocation : MonoBehaviour
+{
+    public LocationInfo Info;
+	public float latitude;
+	public float longitude;
+	public WeatherData weatherData;
+	private string IPAddress;
+	void Start() {
+		StartCoroutine (GetIP());
+	}
+	private IEnumerator GetIP()
+	{
+		var www = new UnityWebRequest("https://ipapi.co/")
+		{
+			downloadHandler = new DownloadHandlerBuffer()
+		};
+
+		yield return www.SendWebRequest();
+
+		if (www.isNetworkError || www.isHttpError)
+		{
+			//error
+			yield break;
+		}
+
+		IPAddress = www.downloadHandler.text;
+		StartCoroutine (GetCoordinates());
+	}
+
+	private IEnumerator GetCoordinates()
+	{
+		var www = new UnityWebRequest("https://ipapi.co/json/" + IPAddress)
+		{
+			downloadHandler = new DownloadHandlerBuffer()
+		};
+
+		yield return www.SendWebRequest();
+
+		if (www.isNetworkError || www.isHttpError)
+		{
+			//error
+			yield break;
+		}
+
+		Info = JsonUtility.FromJson<LocationInfo>(www.downloadHandler.text);
+		latitude = Info.lat;
+		longitude = Info.lon;
+		weatherData.Begin ();
+	}
+}
+
+[Serializable]
+public class LocationInfo
+{
+	public string status;
+	public string country;
+	public string countryCode;
+	public string region;
+	public string regionName;
+	public string city;
+	public string zip;
+	public float lat;
+	public float lon;
+	public string timezone;
+	public string isp;
+	public string org;
+	public string @as;
+	public string query;
+}
