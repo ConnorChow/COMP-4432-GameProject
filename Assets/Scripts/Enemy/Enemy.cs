f4cbe34df9ea355a7a11e9a9f7b9502b3dab095c
ConnorChow <connorjameschow@gmail.com>
2023-04-03 03:02:03 -0400

    Enemy updates
    Enemy now animates properly and I am slowly laying down the groundwork for it to also exhibit flank & strike behaviour in attack mode

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -18,7 +18,10 @@ public class Enemy : NetworkBehaviour {
     private Rigidbody2D rb;
 
     //List of players close to the enemy
-    [SerializeField]public readonly SyncHashSet<GameObject> playersDetected = new SyncHashSet<GameObject>();
+    [SerializeField]public readonly HashSet<GameObject> playersDetected = new HashSet<GameObject>();
+    //List of fellow enemies nearby
+    [SerializeField]public readonly HashSet<GameObject> comradesNearby = new HashSet<GameObject>();
+
     //Player that is being targeted
     private GameObject playerTarget = null;
     //Player's script (to track health)
@@ -44,6 +47,7 @@ public class Enemy : NetworkBehaviour {
 
     //Player Sprite
     [SerializeField] SpriteRenderer enemySprite;
+    [SerializeField] Animator enemyAnimator;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
@@ -55,8 +59,13 @@ public class Enemy : NetworkBehaviour {
         landscape = GameObject.Find("Landscape").GetComponent<LandscapeSimulator>();
 
         playerEnemyTracker = GameObject.Find("CharacterTracker").GetComponent<PlayerEnemyTracker>();
+        if (playerEnemyTracker != null ) {
+            playerEnemyTracker.enemies.Add(gameObject);
+        }
 
         spawnPosition = transform.position;
+
+        flankPatienceTimer = UnityEngine.Random.Range(0.5f, flankingPatience);
     }
 
     private void Update() {
@@ -86,25 +95,31 @@ public class Enemy : NetworkBehaviour {
         }
     }
     [Header("AI States")]
-    //incurred to direct whether to remain idle or to Attack the nearest player
+    public ProtectedInt32 behaviourState = 0; //incurred to direct whether to remain idle or to Attack the nearest player
     // 0 = idle
+    static ProtectedInt32 idle = 0;
     // 1 = attack
-    public ProtectedInt32 behaviourState = 0;
-    //incurred only when behaviourState = 1
+    static ProtectedInt32 attack = 1;
+
+    public ProtectedInt32 attackState; //incurred only when behaviourState = attack
     // 0 = confront
+    static ProtectedInt32 confront = 0;
     // 1 = flank
+    static ProtectedInt32 flank = 1;
     // 2 = charge
+    static ProtectedInt32 charge = 2;
     // 3 = quick retreat
-    public ProtectedInt32 attackState;
+    static ProtectedInt32 retreat = 3;
+
+    //Determine which behaviour the player should explore
     private void AIBehaviour() {
-        switch (behaviourState) {
-            case 0:
-                IdleBehaviour();
-                break;
-            case 1:
-                break;
+        if (behaviourState == idle) {
+            IdleBehaviour();
+        } else if (behaviourState == attack) {
+            AttackBehaviour();
         }
     }
+    //******************************************* Idle Behaviour *******************************************
     [Header("Idle Patrol Data")]
     [SerializeField] ProtectedFloat patrolRadius;
     [SerializeField]ProtectedFloat waitIdlyTime = 2;
@@ -121,8 +136,7 @@ public class Enemy : NetworkBehaviour {
         }
         playerTarget = DetectNearestPlayerInRadius();
         if (playerTarget != null) {
-            playerScript = playerTarget.GetComponent<Player>();
-            behaviourState = 1;
+            TriggerAttackMode(playerTarget);
         }
     }
 
@@ -130,6 +144,66 @@ public class Enemy : NetworkBehaviour {
         Vector3 point = new Vector3(center.x + UnityEngine.Random.Range(-radius, radius), center.y + UnityEngine.Random.Range(-radius, radius), 0);
         return point;
     }
+
+    public void TriggerAttackMode(GameObject player) {
+        if (behaviourState == 1) return;
+        playerTarget = player;
+        playerScript = playerTarget.GetComponent<Player>();
+        behaviourState = 1;
+        attackState = confront;
+        AlertFriends();
+    }
+
+    void AlertFriends() {
+        foreach (GameObject friend in comradesNearby) {
+            if (friend != null) {
+                friend.GetComponent<Enemy>().TriggerAttackMode(gameObject);
+            } else {
+                comradesNearby.Remove(friend);
+            }
+        }
+    }
+    //******************************************* Attack Behaviour *******************************************
+    void AttackBehaviour() {
+        if (playerTarget == null || playerScript == null) {
+            behaviourState = 0;
+            playerTarget = DetectNearestPlayerInRadius();
+            if (playerTarget != null) {
+                TriggerAttackMode(playerTarget);
+            } else {
+                return;
+            }
+        } else {
+            //Set behaviour based on attack mode
+            if (attackState == confront)
+                ConfrontPhase();
+            else if (attackState == flank)
+                FlankPhase();
+            else if (attackState == charge)
+                ChargePhase();
+            else if (attackState == retreat)
+                RetreatPhase();
+        }
+    }
+    //******************************************* Attack Phases *******************************************
+    void ConfrontPhase() {
+
+    }
+
+    [SerializeField] ProtectedFloat flankingPatience = 2; //Highest level of patience to attempt flanking the player before attacking
+    ProtectedFloat flankPatienceTimer;
+    void FlankPhase() {
+
+    }
+    void ChargePhase() {
+
+    }
+    void RetreatPhase() {
+
+    }
+
+    //******************************************* Movement Behaviour *******************************************
+    //allows for very simple moveto command to inform the entity to go somewhere
     [Header("Movement Data")]
     //is the enemy currently supposed to be moving somewhere?
     ProtectedBool moveTo;
@@ -149,6 +223,7 @@ public class Enemy : NetworkBehaviour {
             moveTo = false;
             reachedLoc = true;
             rb.velocity = Vector3.zero;
+            enemyAnimator.SetBool("Walking", false);
             return;
         } else if (rb.transform.position == lastFrameLoc) {
             if (stuckWarnings >= 100) {
@@ -157,9 +232,10 @@ public class Enemy : NetworkBehaviour {
                 moveTo = false;
                 reachedLoc = false;
                 rb.velocity = Vector3.zero;
+                enemyAnimator.SetBool("Walking", false);
                 return;
             } else {
-                stuckWarnings++;
+                if (Time.timeScale > 0) stuckWarnings++;
             }
         } else {
             stuckWarnings = 0;
@@ -169,6 +245,7 @@ public class Enemy : NetworkBehaviour {
         rb.MoveRotation(angle);
         moveDirection = direction;
         rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+        enemyAnimator.SetBool("Walking", true);
     }
 
     //Search through the hashset to try and find the nearest player that has entered the enemy's radius
@@ -200,16 +277,20 @@ public class Enemy : NetworkBehaviour {
         Player player = collision.gameObject.GetComponent<Player>();
         if (player != null) {
             playersDetected.Add(player.gameObject);
-            if (playerTarget == null || playerScript == null) {
-                playerTarget = player.gameObject;
-                playerScript = player;
-            }
+        }
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null) {
+            comradesNearby.Add(enemy.gameObject);
         }
     }
     private void OnTriggerExit2D(Collider2D collision) {
         Player player = collision.gameObject.GetComponent<Player>();
         if (player != null) {
             playersDetected.Remove(player.gameObject);
+        }
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null) {
+            comradesNearby.Remove(enemy.gameObject);
         }
     }
 }
