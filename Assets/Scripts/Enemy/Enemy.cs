eac6c7366413f72dc848d8b085c48d04094079ad
ConnorChow <connorjameschow@gmail.com>
2023-04-03 01:54:23 -0400

    Finished Idle Behaviour

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -3,25 +3,35 @@ using OPS.AntiCheat.Field;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Enemy : NetworkBehaviour {
     public static event Action<Enemy> OnEnemyKilled;
+    [Header("Initial Data")]
     [SerializeField] ProtectedInt32 health, maxHealth = 4;
 
     //movement data
-    [SerializeField] ProtectedFloat moveSpeed = 4;
+    [SerializeField] ProtectedFloat moveSpeed = 3;
+    [SerializeField] ProtectedFloat regularSpeed = 3;
     [SerializeField] ProtectedFloat chargeSpeed = 5;
     private Rigidbody2D rb;
 
+    //List of players close to the enemy
+    [SerializeField]public readonly SyncHashSet<GameObject> playersDetected = new SyncHashSet<GameObject>();
     //Player that is being targeted
-    private GameObject playerTarget;
+    private GameObject playerTarget = null;
     //Player's script (to track health)
-    private Player playerScript;
+    private Player playerScript = null;
 
+    //Patrol data
+    private Vector3 spawnPosition;
+
+    //Data for enemy movement
     private Vector3 target;
     private Vector2 moveDirection;
 
+    [Header("Damage-related Data")]
     //Health Damage Buffer... doesn't actually buffer damage just for showing visual damage
     [SerializeField] ProtectedFloat dmgBufferInterval = 1;
     ProtectedFloat dmgBuffer;
@@ -45,6 +55,8 @@ public class Enemy : NetworkBehaviour {
         landscape = GameObject.Find("Landscape").GetComponent<LandscapeSimulator>();
 
         playerEnemyTracker = GameObject.Find("CharacterTracker").GetComponent<PlayerEnemyTracker>();
+
+        spawnPosition = transform.position;
     }
 
     private void Update() {
@@ -61,6 +73,7 @@ public class Enemy : NetworkBehaviour {
         } else {
             enemySprite.color = Color.white;
         }
+        if (landscape == null) return;
         if (fireCheck > 0) {
             fireCheck -= Time.deltaTime;
         } else {
@@ -72,29 +85,52 @@ public class Enemy : NetworkBehaviour {
             fireCheck = fireCheckInterval;
         }
     }
+    [Header("AI States")]
     //incurred to direct whether to remain idle or to Attack the nearest player
     // 0 = idle
     // 1 = attack
-    public ProtectedInt32 behaviourState;
+    public ProtectedInt32 behaviourState = 0;
     //incurred only when behaviourState = 1
     // 0 = confront
     // 1 = flank
     // 2 = charge
+    // 3 = quick retreat
     public ProtectedInt32 attackState;
     private void AIBehaviour() {
         switch (behaviourState) {
             case 0:
+                IdleBehaviour();
                 break;
-            default:
+            case 1:
                 break;
         }
     }
-
-    ProtectedFloat waitIdlyTime;
+    [Header("Idle Patrol Data")]
+    [SerializeField] ProtectedFloat patrolRadius;
+    [SerializeField]ProtectedFloat waitIdlyTime = 2;
+    ProtectedFloat idleTimer;
     void IdleBehaviour() {
-
+        if (!moveTo) {
+            if (idleTimer > 0) {
+                idleTimer -= Time.deltaTime;
+            } else {
+                MoveTo(FindRandomPointInRadius(spawnPosition, patrolRadius));
+                idleTimer = waitIdlyTime;
+                moveSpeed = regularSpeed;
+            }
+        }
+        playerTarget = DetectNearestPlayerInRadius();
+        if (playerTarget != null) {
+            playerScript = playerTarget.GetComponent<Player>();
+            behaviourState = 1;
+        }
     }
 
+    Vector3 FindRandomPointInRadius(Vector3 center, float radius) {
+        Vector3 point = new Vector3(center.x + UnityEngine.Random.Range(-radius, radius), center.y + UnityEngine.Random.Range(-radius, radius), 0);
+        return point;
+    }
+    [Header("Movement Data")]
     //is the enemy currently supposed to be moving somewhere?
     ProtectedBool moveTo;
     //Has the enemy reached their location?
@@ -112,33 +148,42 @@ public class Enemy : NetworkBehaviour {
         if (Vector3.Distance(target, rb.transform.position) < MoveToTolerance) {
             moveTo = false;
             reachedLoc = true;
+            rb.velocity = Vector3.zero;
+            return;
         } else if (rb.transform.position == lastFrameLoc) {
-            if (stuckWarnings > 100) {
+            if (stuckWarnings >= 100) {
+                Debug.Log("Bad Path, ending moveto");
                 stuckWarnings = 0;
                 moveTo = false;
                 reachedLoc = false;
+                rb.velocity = Vector3.zero;
+                return;
+            } else {
+                stuckWarnings++;
             }
+        } else {
+            stuckWarnings = 0;
         }
         Vector3 direction = (target - transform.position).normalized;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        rb.rotation = angle;
+        rb.MoveRotation(angle);
         moveDirection = direction;
         rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
     }
 
-    /*void HandleMovement() { 
-        if (target) {
-            Vector3 direction = (target.position - transform.position).normalized;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            rb.rotation = angle;
-            moveDirection = direction;
+    //Search through the hashset to try and find the nearest player that has entered the enemy's radius
+    GameObject DetectNearestPlayerInRadius() {
+        if (playersDetected.Count == 0) return null;
+        GameObject nearestPlayer = playersDetected.ElementAt(0);
+        foreach (GameObject player in playersDetected) {
+            float newDistance = Vector3.Distance(transform.position, player.transform.position);
+            float currentDistance = Vector3.Distance(transform.position, nearestPlayer.transform.position);
+            if (newDistance < currentDistance) {
+                nearestPlayer = player;
+            }
         }
-
-        if (target) {
-            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
-        }
-
-    }*/
+        return nearestPlayer;
+    }
 
     public void TakeDamage(int damageAmount) {
         health -= damageAmount;
@@ -151,5 +196,20 @@ public class Enemy : NetworkBehaviour {
         }
     }
 
-
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player != null) {
+            playersDetected.Add(player.gameObject);
+            if (playerTarget == null || playerScript == null) {
+                playerTarget = player.gameObject;
+                playerScript = player;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision) {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player != null) {
+            playersDetected.Remove(player.gameObject);
+        }
+    }
 }
