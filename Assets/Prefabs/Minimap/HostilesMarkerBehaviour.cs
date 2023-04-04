bfdd5d8438c9f22aef6c68cb826813a4e14452a9
ConnorChow <connorjameschow@gmail.com>
2023-04-04 17:39:11 -0400

    minimap does not crash game

--- Assets/Prefabs/Minimap/HostilesMarkerBehaviour.cs
+++ Assets/Prefabs/Minimap/HostilesMarkerBehaviour.cs
@@ -10,14 +10,17 @@ public class HostilesMarkerBehaviour : NetworkBehaviour {
     // Start is called before the first frame update
     void Start() {
         terrainSize = GameObject.Find("Landscape").GetComponent<LandscapeSimulator>().TerrainSize;
+        rtrans = GetComponent<RectTransform>();
     }
 
+    RectTransform rtrans;
     // Update is called once per frame
     void Update() {
         if (!isLocalPlayer) return;
         if (marker != null) {
             float posx = mapDimensions * (marker.transform.position.x / terrainSize + 0.5f);
             float posy = mapDimensions * (marker.transform.position.x / terrainSize + 0.5f);
+            rtrans.transform.position = new Vector2 (posx, posy);
         } else {
             Destroy(gameObject);
         }
